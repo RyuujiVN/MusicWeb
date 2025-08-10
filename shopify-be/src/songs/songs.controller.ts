2b206e3ac7acf65f2368e2aae456d89c4d647b60
Long <nguyenlong14052004@gmail.com>
2025-08-10 18:27:01 +0700

    Create Song

--- shopify-be/src/songs/songs.controller.ts
+++ shopify-be/src/songs/songs.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateSongDTO } from './dtos/create-songs-dto';
 import { SongsService } from './songs.service';
+import { Song } from './song.entity';
 
 @Controller('songs')
 export class SongsController {
   constructor(private readonly songService: SongsService) {}
   @Post()
-  create(@Body() createSongDto: CreateSongDTO) {
+  create(@Body() createSongDto: CreateSongDTO): Promise<Song> {
     return this.songService.create(createSongDto);
   }
 }
